b590d8395cb8c6c736abfc2a60e7f694168c4bd0
TARUN KUMAR B <161626255+Burra-Tarun-Kumar@users.noreply.github.com>
2025-01-08 22:03:33 +0530

    Update 8-01-25-2.py
    Square of numbers

--- 8-01-25-2.py
+++ 8-01-25-2.py
@@ -1,10 +1,5 @@
-# Step 1: Define the function calculate_square
 def calculate_square(n):
     return n * n
-
-# Step 2: In the main program, ask the user to input a positive integer
 n = int(input("Enter a positive integer: "))
-
-# Step 3: Call the calculate_square function and display the result
 square = calculate_square(n)
 print(f"The square of {n} is: {square}")
